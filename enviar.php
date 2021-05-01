f48affca1ad2134419231668e4fbe80508b76dca
Rloria7 <38993189+Rloria7@users.noreply.github.com>
2021-04-30 20:59:46 -0600

    somethings

--- enviar.php
+++ enviar.php
@@ -1,17 +1,17 @@
 <?php
 // llamndo a los campos
-$nombre = $_POST["nombre"];
-$correo = $_POST["correo"];
-$mensaje = $_POST["mensaje"];
+$name = $_POST["name"];
+$mail = $_POST["mail"];
+$message = $_POST["message"];
 
 // datos para el correo
 
 $destinatario = "rloria7@gmail.com";
 $asunto = "mensaje desde ricardoloria.tech";
 
-$carta = "de: $nombre \n";
-$carta .="correo: $correo \n";
-$carta .="mensaje $mensaje";
+$carta = "de: $name \n";
+$carta .="mail: $mail \n";
+$carta .="message $mssage";
 
 // enviando mensaje
 mail($destinatario, $asunto, $carta);
